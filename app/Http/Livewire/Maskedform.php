13193d529c71b23aadcebdf446db7b6f881ffb3c
phph1509 <raphaelph1509@gmail.com>
2021-10-14 22:27:49 -0300

    TA FUNCIONANDO? N SEI MAS TA AI SEJA deus

---
+++ app/Http/Livewire/Maskedform.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Maskedform extends Component
+{
+    public function render()
+    {
+        return view('livewire.maskedform');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <livewire:styles>
 
         @livewireStyles
 
@@ -27,7 +29,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
@@ -42,5 +44,13 @@
         @stack('modals')
 
         @livewireScripts
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <livewire:scripts>
+
+
     </body>
 </html>
